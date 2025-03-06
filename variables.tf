d952521571b4fef4293bf7ab078e00786a9d52d9
Mohammad Quanit <muhammad.quanit@securiti.ai>
2025-03-06 14:41:02 +0500

    PRIV-171688/updated oci templates

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "securiti_token" {
   description = "Token to update pod IP."
-  type = text
 }
 
 variable "securiti_endpoint" {
+  default     = "https://app.securiti.ai"
   description = "Securiti URL Endpoint."
 }
 
-variable "callback_id" {
-  description = "Securiti URL Endpoint Callback ID."
+variable "connector_id" {
+  description = "Connector Id"
 }
